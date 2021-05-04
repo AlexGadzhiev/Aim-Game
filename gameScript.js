19f1efd421c7bf9c0659e2a485bb78c90cf3fa0b
gadzhiev <gadzhiev@iqmen.ru>
2021-05-04 15:35:09 +0300

    increasing counter of result

--- gameScript.js
+++ gameScript.js
@@ -5,24 +5,31 @@ let clickPositionY = -1;
 let count = 0;
 can.width = window.innerWidth;
 can.height = window.innerHeight;
+const rectangle = {
+    width: 50,
+    height: 50,
+};
 function f() {
-    isClicked = false;
     let x = Math.random() * 1850;
     let y = Math.random() * 850;
     function create() {
         ctx.fillStyle = 'black';
-        ctx.fillRect(x, y, 50, 50);
+        ctx.fillRect(x, y, rectangle.width, rectangle.height);
     }
     function clear(){
         ctx.fillStyle = 'white';
-        ctx.fillRect(x - 1, y - 1, 60, 60);
+        ctx.fillRect(x - 1, y - 1, rectangle.width + 10, rectangle.height + 10);
     }
     create();
     can.addEventListener('click', function(e){
         clickPositionX = e.clientX;
         clickPositionY = e.clientY;
-        console.log(document.getElementById('counter').innerHTML++);
-        if(clickPositionX >= x && clickPositionX <= x + 50 && clickPositionY >= y && clickPositionY <= y + 50){
+        console.log("clickPositionX " + clickPositionX);
+        console.log("clickPositionY " + clickPositionY);
+        console.log("x " + x);
+        console.log("y " + y);
+        if(clickPositionX >= x && clickPositionX <= x + 50 && clickPositionY >= y + 63 && clickPositionY <= y + 112){
+            document.getElementById('counter').innerHTML++;
             clear();
             x = -100;
             y = -100;
